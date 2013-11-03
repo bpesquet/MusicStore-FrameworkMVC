444003a0406a59008e38b1fadfbe899abdddc8fa
bpesquet <bpesquet@gmail.com>
2013-11-03 22:18:48 +0100

    Formatage code

--- Modele/Genre.php
+++ Modele/Genre.php
@@ -1,20 +1,24 @@
 <?php
 
 require_once 'Framework/Modele.php';
+
 /**
  * Fournit les services d'accès aux genres musicaux
  *
  * @author Baptiste Pesquet
  */
-class Genre extends Modele {
+class Genre extends Modele
+{
 
-    public function getGenres() {
+    public function getGenres()
+    {
         $sql = "select G.GEN_ID as id, GEN_NOM as nom, count(ALB_ID) AS nbAlbums " .
                 "from T_GENRE G left join T_ALBUM A on G.GEN_ID=A.GEN_ID group by G.GEN_ID order by GEN_NOM";
         return $this->executerRequete($sql);
     }
 
-    public function getGenre($id) {
+    public function getGenre($id)
+    {
         $sql = "select GEN_NOM as nom from T_GENRE where GEN_ID=?";
         $stmtResultats = $this->executerRequete($sql, array($id));
         if ($stmtResultats->rowCount() > 0) {
